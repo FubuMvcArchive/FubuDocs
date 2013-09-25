4eae7b856ab4209e95e1096524390390f74d817b
jeremydmiller <jeremydmiller@yahoo.com>
2013-09-25 08:36:14 -0500

    coded the TopicToken logic for determining paths

---
+++ src/FubuDocs/Tools/DeltaCollector.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FubuDocs.Tools
+{
+    public class DeltaCollector
+    {
+        public readonly IList<IDelta> _deltas = new List<IDelta>(); 
+
+        public DeltaCollector(TopicToken original, TopicToken @new)
+        {
+            @new.DeterminePaths(original.Folder);
+
+            findDeletedTopics(original, @new);
+            findNewAndDeletedFolders(original, @new);
+
+            @new.All().Each(x => {
+                var existing = original.Find(x.Id); // will be null if new
+                var topicDeltas = x.DetermineDeltas(existing);
+
+                _deltas.AddRange(topicDeltas);
+            });
+
+        }
+
+        private void findNewAndDeletedFolders(TopicToken original, TopicToken @new)
+        {
+            var oldFolders = original.All().Select(x => x.Folder).Distinct().ToList();
+            var newFolders = @new.All().Select(x => x.Folder).Distinct().ToList();
+
+            var deletedFolders = oldFolders.Where(x => !newFolders.Contains(x)).Select(x => new DeleteFolder(x));
+            _deltas.AddRange(deletedFolders);
+
+            var addedFolders = newFolders.Where(x => !oldFolders.Contains(x)).Select(x => new CreateFolder(x));
+            _deltas.AddRange(addedFolders);
+        }
+
+        private void findDeletedTopics(TopicToken original, TopicToken @new)
+        {
+            var deletedTopics = original.All().Where(x => @new.Find(x.Id) == null)
+                .Select(x => new DeleteTopic(x));
+            _deltas.AddRange(deletedTopics);
+        }
+
+        public IEnumerable<IDelta> OrderedDeltas()
+        {
+            return groups().SelectMany(x => x).ToArray();
+        }
+
+        private IEnumerable<IEnumerable<IDelta>> groups()
+        {
+            yield return _deltas.OfType<DeleteTopic>();
+            yield return _deltas.OfType<DeleteFolder>();
+            yield return _deltas.OfType<CreateFolder>().OrderBy(x => x.Folder);
+            yield return _deltas.OfType<NewTopic>();
+            yield return _deltas.OfType<MoveTopic>();
+            yield return _deltas.OfType<RewriteTitle>();
+            yield return _deltas.OfType<RewriteUrl>();
+        }
+
+        public void ExecuteDeltas()
+        {
+            var deltas = OrderedDeltas();
+
+            deltas.Each(x => x.Prepare());
+            deltas.Each(x => {
+                Console.WriteLine(x);
+                x.Execute();
+            });
+        }
+    }
+}
--- src/FubuDocs/Tools/TopicToken.cs
+++ src/FubuDocs/Tools/TopicToken.cs
@@ -16,7 +16,7 @@ namespace FubuDocs.Tools
         public TopicToken(Topic topic)
         {
             File = topic.File.FilePath.ToFullPath();
-            Key = Path.GetFileNameWithoutExtension(File);
+            Key = IsIndex ? "index" : topic.Key.Split('/').Last().Split('.').Last();
             FullKey = topic.Key;
             Title = topic.Title;
             Url = topic.Url;
@@ -172,76 +172,23 @@ namespace FubuDocs.Tools
             return new TopicToken(root.Index);
         }
 
-        public void DeterminePaths(string folder)
+        public void DeterminePaths(string containingFolder)
         {
-            throw new NotImplementedException();
-        }
-    }
+            assignOrders();
+            var filename = "{0}.{1}".ToFormat(Order, Key);
 
-    public class DeltaCollector
-    {
-        public readonly IList<IDelta> _deltas = new List<IDelta>(); 
+            if (IsIndex)
+            {
+                var childFolder = containingFolder.AppendPath(filename);
+                File = childFolder.AppendPath("index.spark");
 
-        public DeltaCollector(TopicToken original, TopicToken @new)
-        {
-            @new.DeterminePaths(original.Folder);
-
-            findDeletedTopics(original, @new);
-            findNewAndDeletedFolders(original, @new);
-
-            @new.All().Each(x => {
-                var existing = original.Find(x.Id); // will be null if new
-                var topicDeltas = x.DetermineDeltas(existing);
-
-                _deltas.AddRange(topicDeltas);
-            });
-
-        }
-
-        private void findNewAndDeletedFolders(TopicToken original, TopicToken @new)
-        {
-            var oldFolders = original.All().Select(x => x.Folder).Distinct().ToList();
-            var newFolders = @new.All().Select(x => x.Folder).Distinct().ToList();
-
-            var deletedFolders = oldFolders.Where(x => !newFolders.Contains(x)).Select(x => new DeleteFolder(x));
-            _deltas.AddRange(deletedFolders);
-
-            var addedFolders = newFolders.Where(x => !oldFolders.Contains(x)).Select(x => new CreateFolder(x));
-            _deltas.AddRange(addedFolders);
-        }
-
-        private void findDeletedTopics(TopicToken original, TopicToken @new)
-        {
-            var deletedTopics = original.All().Where(x => @new.Find(x.Id) == null)
-                .Select(x => new DeleteTopic(x));
-            _deltas.AddRange(deletedTopics);
-        }
-
-        public IEnumerable<IDelta> OrderedDeltas()
-        {
-            return groups().SelectMany(x => x).ToArray();
-        }
-
-        private IEnumerable<IEnumerable<IDelta>> groups()
-        {
-            yield return _deltas.OfType<DeleteTopic>();
-            yield return _deltas.OfType<DeleteFolder>();
-            yield return _deltas.OfType<CreateFolder>().OrderBy(x => x.Folder);
-            yield return _deltas.OfType<NewTopic>();
-            yield return _deltas.OfType<MoveTopic>();
-            yield return _deltas.OfType<RewriteTitle>();
-            yield return _deltas.OfType<RewriteUrl>();
-        }
-
-        public void ExecuteDeltas()
-        {
-            var deltas = OrderedDeltas();
-
-            deltas.Each(x => x.Prepare());
-            deltas.Each(x => {
-                Console.WriteLine(x);
-                x.Execute();
-            });
+                Children.Each(x => x.DeterminePaths(childFolder));
+            }
+            else
+            {
+                File = containingFolder.AppendPath(filename);
+                Children.Each(x => x.DeterminePaths(containingFolder));
+            }
         }
     }
 }
