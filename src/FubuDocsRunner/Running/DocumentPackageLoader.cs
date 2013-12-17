6d032c97e9d387729d9a0439827ffa500160afee
jeremydmiller <jeremydmiller@yahoo.com>
2013-12-17 14:00:09 -0600

    closes GH-54

--- src/FubuDocsRunner/Running/DocumentPackageLoader.cs
+++ src/FubuDocsRunner/Running/DocumentPackageLoader.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Bottles;
 using Bottles.Diagnostics;
 using Bottles.Manifest;
+using Bottles.PackageLoaders.Assemblies;
 using FubuCore;
 using FubuDocs.Infrastructure;
 using FubuDocs.Topics;
+using Spark.Compiler.NodeVisitors;
 
 namespace FubuDocsRunner.Running
 {
@@ -27,13 +31,44 @@ namespace FubuDocsRunner.Running
 
             return docDirs
 				.Where(BottlesFilter.ShouldLoad)
-				.Select(dir => {
-                    Console.WriteLine("Loading documentation from folder " + dir);
-				    var pak = reader.LoadFromFolder(dir).As<PackageInfo>();
-                    pak.RemoveAllAssemblies();
-				    
-                    return pak;
-				});
+				.SelectMany(dir => loadPackageFromDirectory(dir, reader));
+        }
+
+        private static IEnumerable<IPackageInfo> loadPackageFromDirectory(string dir, PackageManifestReader reader)
+        {
+            Console.WriteLine("Loading documentation from folder " + dir);
+            var pak = reader.LoadFromFolder(dir).As<PackageInfo>();
+            pak.RemoveAllAssemblies();
+
+
+            var assemblyFiles = new FileSystem().FindFiles(dir,
+                FileSet.Deep("*.Docs.dll", Path.GetFileName(dir) + ".dll"));
+            foreach (var file in assemblyFiles)
+            {
+                var assemblyName = Path.GetFileNameWithoutExtension(file);
+                var assembly =
+                    AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == assemblyName);
+
+                if (assembly == null)
+                {
+                    try
+                    {
+                        assembly = Assembly.LoadFrom(file);
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                if (assembly != null && BottlesFilter.ShouldLoad(assembly))
+                {
+                    yield return new AssemblyPackageInfo(assembly);
+                }
+            }
+
+            yield return pak;
+
+
         }
     }
 }
